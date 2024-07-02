eee3ecdc20e7014115a480ed76d054e8cf286a20
Nishant <bhusalnishant93@gmail.com>
2024-07-02 10:52:30 -0400

    intial

---
+++ src/main/java/com/example/jwtlab2/Jwtlab2Application.java
@@ -0,0 +1,13 @@
+package com.example.jwtlab2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jwtlab2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Jwtlab2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/jwtlab2/Jwtlab2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jwtlab2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Jwtlab2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
